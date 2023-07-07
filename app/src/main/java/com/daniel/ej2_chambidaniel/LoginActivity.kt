a71ef03f929cc67aea25b3fbaba5ca37675f4351
Daniel Chambi Cereceda <danielchambi49@gmail.com>
2023-07-07 16:11:32 -0500

    EC2 commit

---
+++ app/src/main/java/com/daniel/ej2_chambidaniel/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.daniel.ej2_chambidaniel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.text.Editable
+import android.util.Patterns
+import androidx.core.widget.addTextChangedListener
+import com.daniel.ej2_chambidaniel.databinding.ActivityLoginBinding
+import java.util.regex.Pattern
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tilEmail.editText?.addTextChangedListener { text ->
+            binding.btnLogin.isEnabled = validateEmailPassword(text.toString(),binding.tilPassword.editText?.text.toString())
+        }
+
+        binding.tilPassword.editText?.addTextChangedListener { text ->
+            binding.btnLogin.isEnabled = validateEmailPassword(binding.tilEmail.editText?.text.toString(),text.toString())
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    
+    private fun validateEmailPassword(email: String, password: String):Boolean{
+        val isEmailValid=email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches() && email == "Ejemplo@idat.edu.pe"
+        val isPasswordValid= password.isNotEmpty() && password == "123456"
+        return isEmailValid && isPasswordValid
+    }
+
+
+}
